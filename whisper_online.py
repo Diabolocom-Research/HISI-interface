5fecf02f105fb7605d2a59ea112622ee458f5e97
gmaheshwari <saist1993@gmail.com>
2025-05-26 11:00:26 +0200

    added timestamped whisper as an option

--- whisper_online.py
+++ whisper_online.py
@@ -54,6 +54,50 @@ class ASRBase:
         raise NotImplemented("must be implemented in the child class")
 
 
+class WhisperTimestampedASR(ASRBase):
+    """Uses whisper_timestamped library as the backend. Initially, we tested the code on this backend. It worked, but slower than faster-whisper.
+    On the other hand, the installation for GPU could be easier.
+    """
+
+    sep = " "
+
+    def load_model(self, modelsize=None, cache_dir=None, model_dir=None):
+        import whisper
+        import whisper_timestamped
+        from whisper_timestamped import transcribe_timestamped
+        self.transcribe_timestamped = transcribe_timestamped
+        if model_dir is not None:
+            logger.debug("ignoring model_dir, not implemented")
+        return whisper.load_model(modelsize, download_root=cache_dir)
+
+    def transcribe(self, audio, init_prompt=""):
+        result = self.transcribe_timestamped(self.model,
+                                             audio, language=self.original_language,
+                                             initial_prompt=init_prompt, verbose=None,
+                                             condition_on_previous_text=True, **self.transcribe_kargs)
+        return result
+
+    def ts_words(self, r):
+        # return: transcribe result object to [(beg,end,"word1"), ...]
+        o = []
+        for s in r["segments"]:
+            for w in s["words"]:
+                t = (w["start"], w["end"], w["text"])
+                o.append(t)
+        return o
+
+    def segments_end_ts(self, res):
+        return [s["end"] for s in res["segments"]]
+
+    def use_vad(self):
+        self.transcribe_kargs["vad"] = True
+
+    def set_translate_task(self):
+        self.transcribe_kargs["task"] = "translate"
+
+
+
+
 class MLXWhisper(ASRBase):
     """
     Uses MLX Whisper library as the backend, optimized for Apple Silicon.
